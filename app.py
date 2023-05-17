14186a749767898581a64f76b0d62285dbcca9e3
Fernando Chiareli Ferreira <fernando.ferreira.chiareli@hotmail.com>
2023-05-17 17:55:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,46 @@
-from flask import Flask
-from flask import jsonify
-from flask_cors import CORS
-import psycopg2
-#from api.service import service
+#Incio das importacoes
+from flask import Flask#importacao padrao para utilizacao do flask
+from flask import jsonify#para formatar as informações e enviar ao navegador 
+from flask_cors import CORS#importacao padrao para utilizacao do flask
+import psycopg2#importacao para conexao com base de dados postgree
+#fim das importacoes
 
+#inicio do programa
 app = Flask(__name__)
 CORS(app,resources={r"/*":{"origins":"*"}})
-#
-# REGISTRAR AS ROTAS
-#
-#app.register_blueprint(service,url_prefix='/api/service')
 
-def conectar():
-     return psycopg2.connect("dbname=streaming user=postgres password=admin")
+def conectar():# Funcao para se conectar ao banco de dados
+     return psycopg2.connect("dbname=streaming user=postgres password=admin")#estrutura de conexao simples pasando a base de dados o usuario e a senha 
 
 #
-# OPERAÇÕES
+# Inicio das OPERAÇÕES
 #
-@app.route("/")
+@app.route("/")#Operacao no caso / apresentar informações da versão da api
 def info():
     return "API v.1.0"
 
-@app.route("/aue", methods=['GET'])
+@app.route("/teste", methods=['GET'])#Operacao /teste metodo GET select na tabela administrador  
 def teste():
         clientes = []
         try:
-            conn = conectar()
-            cur = conn.cursor()
-            cur.execute("SELECT * FROM administrador")
+            conn = conectar()#conecta ao banco de dados
+            cur = conn.cursor()#utiliza o cur para executar comando no banco 
+            cur.execute("SELECT * FROM administrador")#utiliza o cur.execute para solicitar a execucao de comandos no banco 
 
-            for i in cur.fetchall():
+            for i in cur.fetchall():#A variavel i contem as informacoes do banco em forma de conjunto de elementos 
                 cliente ={}
-                print(i)
+                print(i)#verificacao retirar depois
                 cliente["nome"] = str(i["nome"])
                 cliente["usuario_person_id"] = str(i["usuario_person_id"])
                
-                clientes.append(clientes)
+                clientes.append(clientes)#add a estutura para printar depois
 
         except Exception as e:
-             print(e)
+             print(e)#caso de erro
              clientes=[]
         
-        return jsonify(clientes)
+        return jsonify(clientes)#joga as informacoes na tela
 
-if __name__ == "__main__":
-    app.run(debug=True,host="localhost", port=5000)
+#Estrutura inicial do projeto
+if __name__ == "__main__":#estrutura inicial do projeto
+    app.run(debug=True,host="localhost", port=5000)#local da execucao + porta da execucao lembrando que a base de dados esta na 5432
