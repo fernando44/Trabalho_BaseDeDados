588785b3eb6243a9edc0033ae4b7e1b8020a57c5
Fernando Chiareli Ferreira <fernando.ferreira.chiareli@hotmail.com>
2023-05-18 16:55:37 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@
 #Kevin dourado - 
 
 #Incio das importacoes
-from flask import Flask#importacao padrao para utilizacao do flask
+from flask import Flask, request#importacao padrao para utilizacao do flask
 from flask import jsonify#para formatar as informações e enviar ao navegador 
 from flask_cors import CORS#importacao padrao para utilizacao do flask
 import psycopg2#importacao para conexao com base de dados postgree
@@ -23,6 +23,64 @@ def conectar():# Funcao para se conectar ao banco de dados
 def info():
     return "API v.1.0"
 
+#
+# Adicionar usuario
+#
+@app.route('/dbproj/user', methods=['POST'])#adicionar usuario a base de dados
+def add():
+    users = request.get_json()
+    try:
+        print( type(users['id']))#teste apagar
+        conn = conectar()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO usuario (id, nome, idade, sexo, address, number, saldofinal, premium_status) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", 
+                    (users['id'], users['nome'], users['idade'], users['sexo'], users['address'], users['number'], users['saldofinal'], users['premium_status']) )
+        conn.commit()
+        resposta = jsonify(
+            {
+                'mensagem':'Operacao realizada com sucesso',
+                #'id' : cur.lastrowid
+            }
+        )
+    except Exception as e:
+        conn.rollback()
+        resposta = jsonify({'erro' : str(e)})
+    finally:
+        conn.close()
+    return resposta
+
+#
+# Adicionar musica
+#
+@app.route('/dbproj/album', methods=['POST'])#adicionar usuario a base de dados
+def add():
+     print("Verificar se é um usuario artista\nadicionar a musica")
+     albuns = request.get_json()
+     return 0
+
+#
+# Procurar musica
+#
+@app.route('/dbproj/song/<idMusica>', methods=['GET'])#adicionar usuario a base de dados
+def add():
+     print(idMusica)
+     print("procurar musica pelo id")
+     albuns = request.get_json()
+     return 0
+
+#
+# Procurar artista (info artistas)
+#
+@app.route('/dbproj/artist_info/<idArtista>', methods=['GET'])#adicionar usuario a base de dados
+def add():
+     print(idArtista)
+     print("procurar artista pelo id")
+     albuns = request.get_json()
+     return 0
+
+#
+# apagar depois
+#
 @app.route("/teste", methods=['GET'])#Operacao /teste metodo GET select na tabela administrador  
 def teste():
         clientes = []
@@ -45,6 +103,8 @@ def teste():
         
         return jsonify(clientes)#joga as informacoes na tela
 
+#
 #Estrutura inicial do projeto
+#
 if __name__ == "__main__":#estrutura inicial do projeto
     app.run(debug=True,host="localhost", port=5000)#local da execucao + porta da execucao lembrando que a base de dados esta na 5432
