77079939f5889e1a9050b8717a0a0ee1c2bba31a
Fernando Chiareli Ferreira <fernando.ferreira.chiareli@hotmail.com>
2023-05-19 16:38:51 +0100

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,54 +1,67 @@
+#########################################################
 #Alunos
+#########################################################
 #Fernando Chiareli Ferreira - 
 #Kevin dourado - 
 
+#########################################################
 #Incio das importacoes
+#########################################################
 import uuid
+import os
+import logging as log
 from flask import Flask, request#importacao padrao para utilizacao do flask
 from flask import jsonify
 import flask#para formatar as informações e enviar ao navegador 
 from flask_cors import CORS#importacao padrao para utilizacao do flask
 import psycopg2#importacao para conexao com base de dados postgree
+#########################################################
 #fim das importacoes
+#########################################################
 
+#########################################################
 #inicio do programa
+#########################################################
 app = Flask(__name__)
 CORS(app,resources={r"/*":{"origins":"*"}})
+os.environ['FLASK_ENV'] = 'production'
 
+##########################################################
+## DATABASE ACCESS
+##########################################################
 def conectar():# Funcao para se conectar ao banco de dados
      return psycopg2.connect("dbname=streaming user=postgres password=admin")#estrutura de conexao simples pasando a base de dados o usuario e a senha 
 
-#
+##########################################################
 # Inicio das OPERAÇÕES
-#
+##########################################################
 @app.route("/")#Operacao no caso / apresentar informações da versão da api
 def info():
     return "API v.1.0"
 
-#
+##########################################################
 # Adicionar usuario
-#
+##########################################################
 @app.route('/dbproj/user', methods=['POST'])#adicionar usuario a base de dados
 def add():
     users = flask.request.get_json()
     try:
         # Gera um UUID aleatório
-        id_aleatorio = uuid.uuid4()
+        id = uuid.uuid4()
         # Obtém a representação inteira do UUID
-        id_aleatorio_int = id_aleatorio.int
+        idStr = str(id)
 
         conn = conectar()
         cur = conn.cursor()
         
-        statement = "INSERT INTO usuario (id, nome, idade, sexo, address, number, saldofinal, premium_status) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        values = (id_aleatorio_int, users['nome'], users['idade'], users['sexo'], users['address'], users['number'], users['saldofinal'], users['premium_status'])
+        statement = "INSERT INTO usuario (id, username, idade, sexo, address, number, password) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+        values = (idStr, users['username'], users['idade'], users['sexo'], users['address'], users['number'],  users['password'])
         cur.execute(statement,values)
 
         conn.commit()
         resposta = jsonify(
             {
                 'mensagem':'Operacao realizada com sucesso',
-                #'id' : cur.lastrowid
             }
         )
     except Exception as e:
@@ -58,18 +71,38 @@ def add():
         conn.close()
     return resposta
 
-#
+##########################################################
 # Adicionar musica
-#
+##########################################################
 @app.route('/dbproj/album', methods=['POST'])#adicionar musica a base de dados
 def addMusic():
     print("Verificar se é um usuario artista\nadicionar a musica")
+    
     albuns = request.get_json()
-    return 0
+    try:
+        id = uuid.uuid4()
+        idStr = str(id)
 
-#
+        conn=conectar
+        cur = conn.cursor()
+
+        statement= "INSERT INTO songs (ism, nome, duracao) VALUES (%s, %s, %s)"
+        values=(idStr,albuns['nome'], albuns['duracao'])
+        cur.execute(statement,values)
+
+        conn.commit()
+
+    except Exception as e:
+        conn.rollback()
+        resposta = jsonify({'erro' : str(e)})
+    finally:
+        conn.close()
+    return resposta
+
+
+##########################################################
 # Procurar musica
-#
+##########################################################
 @app.route('/dbproj/song/<ismn>', methods=['GET'])
 def procuraMusica(ismn):
     try:
@@ -107,9 +140,9 @@ def procuraMusica(ismn):
         if conn:
             conn.close()
 
-#
+##########################################################
 # Procurar artista (info artistas)
-#
+##########################################################
 @app.route('/dbproj/artist_info/<idArtista>', methods=['GET'])#adicionar usuario a base de dados
 def procuraArtista():
      print(idArtista)
@@ -117,9 +150,9 @@ def procuraArtista():
      albuns = request.get_json()
      return 0
 
-#
+##########################################################
 # apagar depois
-#
+##########################################################
 @app.route("/teste", methods=['GET'])#Operacao /teste metodo GET select na tabela administrador  
 def teste():
         clientes = []
@@ -142,8 +175,14 @@ def teste():
         
         return jsonify(clientes)#joga as informacoes na tela
 
-#
+##########################################################
 #Estrutura inicial do projeto
-#
+##########################################################
 if __name__ == "__main__":#estrutura inicial do projeto
-    app.run(debug=True,host="localhost", port=5000)#local da execucao + porta da execucao lembrando que a base de dados esta na 5432
+    # Configuração básica do registro
+    log.basicConfig(filename='bdLog.log', level=log.INFO,format='%(asctime)s [%(levelname)s]: %(message)s')
+    host = '127.0.0.1'
+    port = 5000
+    log.info(f'API v1.0 online: http://{host}:{port}')#escrever no arquivo de log
+    app.run(host=host, debug=False, threaded=True, port=port)#local da execucao + porta da execucao lembrando que a base de dados esta na 5432
+    
